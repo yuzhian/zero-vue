c0e70f328afe98d8f478be53be6651cc89eadf9d
yuzhian <yu97@live.com>
2021-09-08 21:03:18 +0800

    全局菜单指令部分功能

--- src/plugins/directives/contextmenu.ts
+++ src/plugins/directives/contextmenu.ts
@@ -1,8 +1,34 @@
+import { createApp } from '@vue/runtime-dom'
+import Contextmenu from '../../components/Contextmenu'
+
+const _WRAPPER_ID = 'z_contextmenu_wrapper'
+
+function loadWrapper(): HTMLElement {
+  let wrapper = document.getElementById(_WRAPPER_ID)
+  if (wrapper) {
+    return wrapper
+  }
+  wrapper = document.createElement('div')
+  wrapper.id = _WRAPPER_ID
+  document.body.appendChild(wrapper)
+  return wrapper
+}
+
+function mountContext(wrapper: HTMLElement) {
+  createApp(Contextmenu).mount(wrapper)
+}
+
+function showContextmenuAt({ clientX, clientY }: MouseEvent) {
+  const wrapper = loadWrapper()
+  wrapper.setAttribute('style', `left: ${clientX}px; top: ${clientY}px; position: relative;`)
+  mountContext(wrapper)
+}
+
 export default {
   mounted(el: HTMLElement) {
     el.addEventListener('contextmenu', e => {
       e.preventDefault()
-      console.log(e.clientX, e.clientY)
+      showContextmenuAt(e)
     })
   },
 }
