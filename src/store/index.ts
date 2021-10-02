53e99153ea70c34264f128e31463219130516168
yuzhian <yu97@live.com>
2021-10-02 12:54:14 +0800

    清理多余的布尔计算

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,7 @@ import { createStore, createLogger, Store } from 'vuex'
 import account from './modules/account'
 import permission from './modules/permission'
 
-const debug: boolean = !import.meta.env.PROD
+const debug: boolean = import.meta.env.DEV
 
 export default createStore({
   modules: { account, permission },
